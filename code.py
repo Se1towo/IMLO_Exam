edec01762d686fcb601988b52f1c7b207d36d7fa
Se1towo <zot500@york.ac.uk>
2024-04-25 22:07:04 +0100

    Importing Flowers 102

---
+++ code.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+import numpy as np
+import pandas as pd
+from torch.utils.data.dataloader import dataloader
+from torch.utils.data import Dataset
+
+torchvision.datasets.Flowers102(root: Union[str, Path], split: str = 'train', transform: Optional[Callable] = None, target_transform: Optional[Callable] = None, download: bool = False)
